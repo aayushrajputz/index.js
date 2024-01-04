03d6ca92c9083557b1b864aa4bd50554da6f4028
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2024-01-04 06:52:19 +0000

    variable $ datatypes

---
+++ 01_basics_js/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "aayushpratapsingh001.com"
+var password =  "384882"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2 not allowed
+
+// console.log(accountId);
+
+accountEmail = "aurhu@.com"
+password  = "yuuh575"
+accouuntCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountCity,accountEmail,password,accountState])
+
+// prefer not to use var 
+// beacuse of some issue in scope and functional scope
---
+++ 01_basics_js/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+
+//alert('hello') not run in node.js
+
+console.log('aayush')
+
+let name = 'aayush'
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+//bigint
+//boolean=true/false
+//string => ""
+//null => standalone value/its a object
+//undefined => value is not assigned 
+
+// symbol => find uniqueness
+
+
+// objects
+
+
+console.log(typeof undefined);//undefined 
+console.log(typeof null);//object
