80b5be9ec3c6bdc2f9f3a576989ef428bf56ee9b
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2024-01-05 06:57:36 +0000

    conversion on datatypes

---
+++ 01_basics_js/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=> 33
+//"33asd"=>NaN
+//True =>1/False =>0
+
+// let isLoggedIn = 'aayush'
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> ture/ 0 => false
+// "" => false
+// "AAYUSH"=> ture
+
+let anyNumber = 33
+
+ let stringNumber = String(anyNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
