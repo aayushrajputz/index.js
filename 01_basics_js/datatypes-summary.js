a61e6b4e24c17f74eda953c40566e85919480f82
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2024-01-05 09:14:32 +0000

    datatype-summary

---
+++ 01_basics_js/datatypes-summary.js
@@ -0,0 +1,49 @@
+// primitive 
+
+//7  types
+
+// String = string
+// Number = number
+// Boolean = boolean
+// Null = object
+// Undefined = undefined
+// Symbol= symbol
+// BigInt = bigint
+
+//  js is a dynamically typed language beacuse
+  // that data type of variable are determined by the value
+   //the hold at runtime
+
+   const score = 100
+   const scoreValue = 100.3
+
+   const isLoggedIn = true
+   const outsideTemp = null
+   let userEmail;
+
+   const id = Symbol('123')
+   const anotherId = Symbol('123')
+   console.log(id === anotherId);
+
+const bigNumber = 234384787383874344873492883982n
+
+
+// reference (non - primivtive)
+
+// array = object
+// object = object
+
+//function= object function
+
+const heros = ["shaktiman","naagraj","dooga"];
+
+let myObj = {
+    name: "aayush",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof id)
