7b9981a94724d52271d0745ea0361a48ae56422f
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2024-04-03 15:35:21 +0530

    Update aayush.js

--- 07_projects/01_project/aayush.js
+++ 07_projects/01_project/aayush.js
@@ -1,6 +1,25 @@
-const buttons = document.querySelectorAll("button")
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll('.button')
+// console.log(buttons)
+const body = document.querySelector("body")
 
-buttons.forEach(function(button){
-    console.log(button);
+buttons .forEach(function(button){
+console.log(button);
+button.addEventListener('click', function(e){
+console.log(e.target)
+if(e.target.id  === 'grey') {
+  body.style.backgroundColor= e.target.id;
+}
+if(e.target.id  === 'white') {
+  body.style.backgroundColor= e.target.id;
+}
+if(e.target.id  === 'blue') {
+  body.style.backgroundColor= e.target.id;
+}
+if(e.target.id  === 'yellow') {
+  body.style.backgroundColor= e.target.id;
+}
+if(e.target.id  === 'black') {
+  body.style.backgroundColor= e.target.id;
+}
 })
+});
